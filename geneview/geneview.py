3ec933cecc6d625b1d9a92b268b5f4b2a618b28c
Matt Ziegler <mpziegler@wisc.edu>
2011-09-16 22:07:28 +0200

    Re-ordered gene-view columns, fixed CSV export

--- geneview/geneview.py
+++ geneview/geneview.py
@@ -1,3 +1,5 @@
+from urllib import quote_plus
+
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404
 from django.db.models import Sum, Count, F
@@ -21,7 +23,7 @@ def search(request):
         form = SearchForm()
     
     # get the order from the query string
-    orderby = request.GET.get('orderby', default='score')
+    orderby = request.GET.get('orderby', default='f1_score')
     
     return render_to_response('genesearch.html', {'form': form, 'q':q, 'orderby':orderby})
 
@@ -50,10 +52,10 @@ def result(request):
     except: limit = 18446744073709551615 # arbitrary large number (no better way to do this.)
     
     # get the order from the query string
-    orderby = request.GET.get('orderby', default='score')
+    orderby = request.GET.get('orderby', default='f1_score')
     # make sure that the order-by option is valid, to prevent SQL injection
-    if orderby not in ('score', 'hits', 'abstracts', 'f1_score'):
-        orderby = 'score'
+    if orderby not in ('precision', 'hits', 'abstracts', 'f1_score'):
+        orderby = 'f1_score'
     
     def paramstring(l):
         """Return a string of comma-separated %s's of length l
@@ -67,7 +69,7 @@ def result(request):
     sqlquery = """
     SELECT g.*, 
         COUNT(*) hits, 
-        COUNT(*)/ (`abstracts` + 10) score,
+        COUNT(*)/ (`abstracts` + 10) `precision`,
         (2 * (COUNT(*) / `abstracts`) * (COUNT(*) / {query_abstracts})) / 
             ((COUNT(*) / `abstracts`) + (COUNT(*) / {query_abstracts})) f1_score
     FROM `gene_abstract` a
@@ -75,7 +77,7 @@ def result(request):
     ON g.id = a.gene
     WHERE a.`abstract_pmid` in ({paramstring})
     GROUP BY g.id
-    ORDER BY {orderby} DESC
+    ORDER BY `{orderby}` DESC
     LIMIT %s, %s;
     """.format(paramstring=paramstring(len(abstracts)), orderby=orderby, query_abstracts=query_abstracts)
     genes = Gene.objects.raw(sqlquery, abstracts + [offset, limit])
@@ -87,7 +89,7 @@ def result(request):
     # return either a CSV (if "download" is in the query string,) an HTML page, or a 404
     if (request.GET.get('download')):
         response = HttpResponse(mimetype='text/csv')
-        response['Content-Disposition'] = 'attachment; filename=gadget-{0}.csv'.format(query)
+        response['Content-Disposition'] = 'attachment; filename=gadget-{0}.csv'.format(quote_plus(query))
         response.write(makeCSV(genes, pvals))
         return response
     else:
@@ -97,10 +99,10 @@ def result(request):
             raise Http404 # no results
         
 def makeCSV(genes, pvals):
-    """Create a CSV file given a list of genes and a list of p values."""
-    header = 'rank,score,p_value,symbol,name,synonyms,entrez_id,hgnc_id,ensembl_id,mim_id,hprd_id,chromosome,map_location\n'
+    """Create a CSV file (returned as a string) given a list of genes and a list of p values."""
+    header = 'rank,f1_score,matching_abstracts,total_abstracts,adjusted_precision,p_value,symbol,name,synonyms,entrez_id,hgnc_id,ensembl_id,mim_id,hprd_id,chromosome,map_location\n'
     body = '\n'.join([','.join(['"{0}"'.format(f) for f in 
-        (rank, '{0:0.3f}'.format(g.score), '{0:0.5f}'.format(p), g.symbol, g.name, g.synonyms, g.entrez_id, g.hgnc_id, g.ensembl_id, g.mim_id, g.hprd_id, g.chromosome, g.maplocation)])
+        (rank, '{0:0.3f}'.format(g.f1_score), g.hits, g.abstracts, '{0:0.3f}'.format(g.precision), p, g.symbol, g.name, g.synonyms, g.entrez_id, g.hgnc_id, g.ensembl_id, g.mim_id, g.hprd_id, g.chromosome, g.maplocation)])
         for rank, (g, p) in enumerate(zip(genes, pvals))])
     return header + body
 
